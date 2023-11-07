4a2ea09fd1285558cce6f8262db2e9fb8fd0b486
George van Heerden <george.vanheerden@sovtech.com>
2023-11-07 08:15:09 +0200

    feat(Videos): add medium length video example

--- src/containers/Videos/index.tsx
+++ src/containers/Videos/index.tsx
@@ -1,7 +1,7 @@
 import { Video } from '../../components/Video'
 import './index.css'
 
-const videos = [
+const shortVideos = [
   {
     name: 'He-Man',
     url: 'https://player.odycdn.com/v6/streams/632b113daa75d9f8a93049286ed56daec45c64fb/508c8c.mp4',
@@ -12,26 +12,29 @@ const videos = [
   },
 ]
 
+const mediumVideos = [
+  {
+    name: 'Looney Tunes',
+    url: 'https://player.odycdn.com/v6/streams/ff43bdc4a96a4dbe30c60f63fa522458f8e51522/33f4d4.mp4',
+  },
+]
+
 function Videos() {
-  // const [cachedVideoUrl, setCachedVideoUrl] = useState<string | null>(null)
-
-  // const videoUrl =
-  //   'https://player.odycdn.com/v6/streams/59b4017390a032f34fb948f51c0a1b325f016223/9b905a.mp4' // HE-MAN
-
-  // // 'https://player.odycdn.com/v6/streams/7124aaec698e758f3ef91c8fd4ee81d818416d62/e06600.mp4' // Transformers
-
-  // useEffect(() => {
-  //   getFileUrl(videoUrl).then((url) => {
-  //     setCachedVideoUrl(url)
-  //   })
-  // }, [])
-
-  // console.log(cachedVideoUrl)
-
   return (
     <div className="vertical-stack">
-      <h1>Videos</h1>
-      {videos.map((video) => (
+      <h1>Short Videos</h1>
+      <h2>2-3 min</h2>
+      {shortVideos.map((video) => (
+        <Video key={video.name} videoName={video.name} videoUrl={video.url} />
+      ))}
+
+      <h1>Medium Videos</h1>
+      <h2>~7 min</h2>
+      <p>
+        These videos will not download, since our current method of caching
+        video data doesn't implement chunking.
+      </p>
+      {mediumVideos.map((video) => (
         <Video key={video.name} videoName={video.name} videoUrl={video.url} />
       ))}
     </div>
